24e2ff19beae2eb1b8fe46154808cf19d91409fe
Glauber Ramos <glauberamos@gmail.com>
2016-05-04 01:45:36 -0300

    adding menu

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 $(function() {
+  $("nav a").click(function(event) {
+    event.preventDefault(event);
+    var session = $(this).attr('href');
+    var sessionPosition = $(session).offset().top;
+
+    console.log(sessionPosition);
+
+    $('body,html').animate({ scrollTop: sessionPosition }, 500, function() {
+        window.location.hash = session;
+    });
+  });
 });
