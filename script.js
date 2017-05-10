c3f201c9cccfa7f54b65869b2a14f16a145cc45e
Glauber Ramos <glauberamos@gmail.com>
2017-05-09 23:36:06 -0300

    Remove not used stuff

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 $(function() {
-  $("nav a:not(.button)").click(function(event) {
-    event.preventDefault(event);
-    var session = $(this).attr('href');
-    var sessionPosition = $(session).offset().top - 40;
-
-    console.log(sessionPosition);
-
-    $('body,html').animate({ scrollTop: sessionPosition }, 500, function() {
-        window.location.hash = session;
-    });
-  });
-
   $("div.lazy").lazyload({
     effect : "fadeIn"
   });
