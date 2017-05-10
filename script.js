4343c650a185db23af8cfd1a9646b14e480c56ee
Glauber Ramos <glauberamos@gmail.com>
2017-05-10 02:00:44 -0300

    Adding amazon images on style.css and new lazy load

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-$(function() {
-  $("div.img").lazyload({
-    effect : "fadeIn"
-  });
-});
+var arrayTopPositions = []
+var wscroll = 0
+var wh = $(window).height()
+
+function updateOffsets() {
+  $('div.img').each(function() {
+    var topPosition = $(this).offset().top
+    arrayTopPositions.push(topPosition)
+  })
+}
+
+function lazyLoad() {
+  wscroll = $(window).scrollTop();
+
+  for (i = 0; i < arrayTopPositions.length; i++) {
+    if (arrayTopPositions[i] <= wscroll + (wh - 200)) {
+      $('div.img').eq(i).addClass('loaded');
+    }
+  }
+}
+
+updateOffsets()
+lazyLoad()
+
+$(window).on('scroll', function() {
+  lazyLoad()
+})
